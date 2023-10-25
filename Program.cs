b3c95d359d12d3ee6ce8a91165682a7b2ceadbc8
Richard Bodén <utjis@hotmail.com>
2023-10-25 11:21:20 +0200

    hej

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ class Program{
     Console.WriteLine("GitTest4 + tog bort en rad");
     Console.WriteLine("Staging Area");
     Console.WriteLine("GitTest5");
+    System.Console.WriteLine("hej git test 6");
     }
 
 }
